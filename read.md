b5f11c52a9d510a6d47e52c36852d2e8c08f1664
Nandish <nandishp.reddy@gmail.com>
2019-10-05 12:15:11 +0530

    second commit

---
+++ read.md
@@ -0,0 +1,3 @@
+# Hello world
+
+This containd test repo app
---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const port = 3010
+
+const server = http.createServer(function(request,response){
+    if(request.url == '/'){
+        response.end('response from node server')
+    }
+    else if(request.url == '/about') {
+        response.end('about page')
+    }else {
+        response.end(' does not exist')
+    }
+})
+
+server.listen(port, function(){
+ console.log('listening on port',port)
+})
